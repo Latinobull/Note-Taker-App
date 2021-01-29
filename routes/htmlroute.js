0fb3954021754e71f0b7601d7e73a7af0cc5c495
Donnahue George <Donnahuegjr@gmail.com>
2021-01-29 12:05:46 -0500

    connected routes to server

--- routes/htmlroute.js
+++ routes/htmlroute.js
@@ -1,12 +1,11 @@
 const path = require("path");
-const OUTPUT = path.resolve(__dirname, "public");
 
 module.exports = function (app) {
   app.get("*", function (req, res) {
-    res.sendFile(path.join(OUTPUT, "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/notes", function (req, res) {
-    res.sendFile(path.join(OUTPUT, "notes.html"));
+    res.sendFile(path.join(__dirname, "../public/note.html"));
   });
 };
