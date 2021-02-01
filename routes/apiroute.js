019b3d313a3ab1d6567be4d7c3972ddeadb33b60
Donnahue George <Donnahuegjr@gmail.com>
2021-02-01 15:12:06 -0500

    got the post route completed

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,4 +1,4 @@
-const { fstat } = require("fs");
+const { fs } = require("fs");
 const path = require("path");
 
 module.exports = function (app) {
@@ -9,6 +9,26 @@ module.exports = function (app) {
       }
       var jsonContent = JSON.parse(data);
       res.json(jsonContent);
+    });
+  });
+
+  app.post("/api/notes", function (req, res) {
+    let posted = req.body;
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+      if (err) {
+        console.error(err);
+      }
+      var idNum = 0;
+      idNum++;
+      var jsonContent = JSON.parse(data);
+      var addedNotes = { title: posted.title, text: posted.text, id: idNum };
+      jsonContent.push(addedNotes);
+
+      var toString = JSON.stringify(jsonContent);
+
+      fs.writeFile("db/db.json", toString, "utf8", function () {
+        res.json(posted);
+      });
     });
   });
 };
