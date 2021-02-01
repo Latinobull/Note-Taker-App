e385a516a3ac095ae19462105c2236d418b42f20
Donnahue George <Donnahuegjr@gmail.com>
2021-02-01 17:15:01 -0500

    cleaning up routes

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,34 +1,18 @@
-const { fs } = require("fs");
-const path = require("path");
+var notes = require("../db/db.json");
+const fs = require("fs");
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-      if (err) {
-        console.error(err);
-      }
-      var jsonContent = JSON.parse(data);
-      res.json(jsonContent);
-    });
+    res.json(notes);
   });
 
   app.post("/api/notes", function (req, res) {
-    let posted = req.body;
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-      if (err) {
-        console.error(err);
-      }
-      var idNum = 0;
-      idNum++;
-      var jsonContent = JSON.parse(data);
-      var addedNotes = { title: posted.title, text: posted.text, id: idNum };
-      jsonContent.push(addedNotes);
-
-      var toString = JSON.stringify(jsonContent);
-
-      fs.writeFile("db/db.json", toString, "utf8", function () {
-        res.json(posted);
-      });
-    });
+    const posted = req.body;
+    var idNum = 0;
+    idNum++;
+    const newNote = { title: posted.title, text: posted.text, id: idNum };
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
   });
 };
